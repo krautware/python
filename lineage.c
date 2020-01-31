01ffd187bf2cb98df0763aa90b8f6eecf38b2068
krautware <friedhelm.kraeutlein@gmx.net>
2020-01-31 20:00:43 +0100

    forgot singular

--- lineage.c
+++ lineage.c
@@ -6,13 +6,18 @@
 #define POPULATION 9000000
 #define ANCESTORS 4001
 #define FREQUENCY 10
+#define OLD 0
+
 
 typedef struct P {
     int ancestor;
     struct P *p;
 } P;
 
-long my_random(void){
+int ancestors[ANCESTORS], counts[ANCESTORS];
+P hash_vec[ANCESTORS];
+
+int my_random(void){
     static long max_member = RAND_MAX - (RAND_MAX % POPULATION);
     long member;
 
@@ -23,95 +28,105 @@ long my_random(void){
 
 int main(int argc, char** argv){
 
-int ancestors[ANCESTORS] = {0}, counts[ANCESTORS] = {0};
-P hash_vec[ANCESTORS] = {0};
+    time_t t = time(NULL);
 
-time_t t = time(NULL);
+    srandom(t);
 
-srandom(t);
+    int candidate;
+    int i, sum, singular, hash;
+    long max = -1;
+    long min = RAND_MAX;
 
-long candidate;
-int i, sum, singular, hash;
-long max = -1;
-long min = RAND_MAX;
+    P *p = NULL;
+    int frequency[FREQUENCY] = {0};
 
-P *p = NULL;
-int frequency[FREQUENCY] = {0};
-
-for(i = 0; i < ANCESTORS; ++i){
-    singular = 1;
+    for(i = 0; i < ANCESTORS; ++i){
+        singular = 1;
     
-    do{
-        candidate = my_random();
-        hash = candidate % ANCESTORS;
+        do{
+            candidate = my_random();
+            hash = candidate % ANCESTORS;
         
-        if(hash_vec[hash].ancestor == 0){
-            hash_vec[hash].ancestor = candidate;
-            counts[hash]++;
-            singular = 0;
-        }
-        else{
-            if(hash_vec[hash].ancestor == candidate){
+            if(hash_vec[hash].ancestor == 0){
+                hash_vec[hash].ancestor = candidate;
+#ifdef OLD
+                counts[hash]++;
+#endif
+                singular = 0;
+            }   
+            else{
+                if(hash_vec[hash].ancestor == candidate){
                     continue;
-            }else{
-                if((hash_vec[hash].ancestor != candidate) && (hash_vec[hash].p == NULL)){
-                    if((hash_vec[hash].p = malloc(sizeof(P))) == NULL){
-                        fprintf(stderr, "No more memory!\n");
-                        exit(1);
-                    }
-                    hash_vec[hash].p->ancestor = candidate;
-                    hash_vec[hash].p->p = NULL;
-                    counts[hash]++;
-                }
-                else{
-                    p = hash_vec[hash].p;
-                    while(p){
-                        if(p->ancestor == candidate)
-                            break;
-                        if(p->p)
-                            p = p->p;
-                        else{
-                            if((p->p = malloc(sizeof(P))) == NULL){
-                                fprintf(stderr, "No more memory!\n");
-                                exit(1);
-                            }
-                            p = p->p;
-                            p->ancestor = candidate;
-                            counts[hash]++;
-                            p->p = NULL;
-                            singular = 0;
-                            p = p->p;
+                }else{
+                    if((hash_vec[hash].ancestor != candidate) && (hash_vec[hash].p == NULL)){
+                        if((hash_vec[hash].p = malloc(sizeof(P))) == NULL){
+                            fprintf(stderr, "No more memory!\n");
+                            exit(1);
                         }
-                    }        
+                        hash_vec[hash].p->ancestor = candidate;
+                        hash_vec[hash].p->p = NULL;
+#ifdef OLD
+                        counts[hash]++;
+                        singular = 0;
+#endif
+                        
+                    }
+                    else{
+                        p = hash_vec[hash].p;
+                        while(p){
+                            if(p->ancestor == candidate)
+                                break;
+                            if(p->p)
+                                p = p->p;
+                            else{
+                                if((p->p = malloc(sizeof(P))) == NULL){
+                                    fprintf(stderr, "No more memory!\n");
+                                    exit(1);
+                                }
+                                p = p->p;
+                                p->ancestor = candidate;
+#ifdef OLD                                                                                     
+                                counts[hash]++;
+                                /* printf("Mehr als einmal\n"); */
+#endif                                
+                                p->p = NULL;
+                                singular = 0;
+                                p = p->p;
+                            }   
+                        }        
+                    }   
                 }
             }
-        }
-    }while(singular);
+        }while(singular);
+#ifndef OLD        
+        counts[hash]++;
+#endif        
+        ancestors[i] = candidate;
+    }
+
+    for(i = 0; i < ANCESTORS; ++i){
+        frequency[counts[i]]++;
+    }
+
+    for(i = 0; i < ANCESTORS; ++i){
+        if(max < ancestors[i])
+            max = ancestors[i];
+        if(min > ancestors[i])
+            min = ancestors[i];
+    }
+
+    printf("\nMIN Wert: %9.ld\nMAX Wert: %9.ld\n", min, max);
+
+    for(sum = i = 0; i < FREQUENCY; ++i){
+        printf("%2d -> %4d --> %4d\n", i, frequency[i], sum += frequency[i]);
+    }
     
-    ancestors[i] = candidate;
-}
+    for(sum = i = 0; i < FREQUENCY; ++i){
+        sum += frequency[i] * i;
+    }
 
-for(i = 0; i < ANCESTORS; ++i){
-    frequency[counts[i]]++;
-}
-
-for(i = 0; i < ANCESTORS; ++i){
-    max = ((max < ancestors[i]) ? ancestors[i] : max);
-    min = ((min > ancestors[i]) ? ancestors[i] : min);
-    printf("%d ", ancestors[i]);
-}
-
-printf("\nMIN Wert: %9.ld\nMAX Wert: %9.ld\n", min, max);
-
-for(sum = i = 0; i < FREQUENCY; ++i){
-    printf("%2d -> %4d --> %4d\n", i, frequency[i], sum += frequency[i]);
-}
-    
-for(sum = i = 0; i < FREQUENCY; ++i){
-    sum += frequency[i] * i;
-}
-
-printf("Gesamt: %d\n", sum);
+    printf("Gesamt: %d\n", sum);
+    printf("Durchschnitt: %.2f\n", (float)sum / (ANCESTORS - frequency[0]));
 
 
 }
